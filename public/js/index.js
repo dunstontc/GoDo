374bb4b580f90ca3eff2e0d8a09f9c99744c1566
Clay Dunston <dunstontc@gmail.com>
2018-09-17 16:11:23 -0500

    index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!',
-    todos: [],
-  }
-})
+$(document).ready(function() {
+  console.log("ready!");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("ready!");
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
